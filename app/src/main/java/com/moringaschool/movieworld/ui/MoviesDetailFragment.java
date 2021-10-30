d267353bcd8d8fa5bb7be5f4c27e855ed479028b
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-10-31 01:26:44 +0300

    Bundles information in the parceler

--- app/src/main/java/com/moringaschool/movieworld/ui/MoviesDetailFragment.java
+++ app/src/main/java/com/moringaschool/movieworld/ui/MoviesDetailFragment.java
@@ -2,65 +2,83 @@ package com.moringaschool.movieworld.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcel;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.moringaschool.movieworld.R;
+import com.moringaschool.movieworld.models.Result;
+import com.moringaschool.movieworld.models.VisionBusiness;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MoviesDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 public class MoviesDetailFragment extends Fragment {
+    @BindView(R.id.restaurantImageView) ImageView mImageLabel;
+    @BindView(R.id.restaurantNameTextView) TextView mNameLabel;
+    @BindView(R.id.cuisineTextView) TextView mCategoriesLabel;
+    @BindView(R.id.ratingTextView) TextView mRatingLabel;
+    @BindView(R.id.websiteTextView) TextView mWebsiteLabel;
+    @BindView(R.id.phoneTextView) TextView mPhoneLabel;
+    @BindView(R.id.addressTextView) TextView mAddressLabel;
+    @BindView(R.id.saveRestaurantButton) TextView mSaveRestaurantButton;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Result mMovies;
+//    private VisionBusiness moviesList;
 
     public MoviesDetailFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MoviesDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MoviesDetailFragment newInstance(String param1, String param2) {
+
+    public static MoviesDetailFragment newInstance(Result movies) {
         MoviesDetailFragment fragment = new MoviesDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putParcelable("movies", Parcels.wrap(movies));
+//        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable  Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        assert getArguments() != null;
+        mMovies = Parcels.unwrap(getArguments().getParcelable("movies"));
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_movies_detail, container, false);
+        ButterKnife.bind(this, view);
+        Picasso.get().load(mMovies.getPosterPath()).into(mImageLabel);
+
+        List<String> categories = new ArrayList<>();
+
+//        for (Locale.Category category: mMovies.get)
+
+        mNameLabel.setText(mMovies.getTitle());
+        mRatingLabel.setText(Double.toString(mMovies.getVoteAverage()));
+        return view;
     }
 }
