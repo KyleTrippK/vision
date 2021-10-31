ec663bab5bab7a9a47e7cda241245e3c95b520ed
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-10-31 17:10:58 +0300

    Adds a viewpager to the Movie details activity

--- app/src/main/java/com/moringaschool/movieworld/ui/MovieDetailsActivity.java
+++ app/src/main/java/com/moringaschool/movieworld/ui/MovieDetailsActivity.java
@@ -1,16 +1,39 @@
 package com.moringaschool.movieworld.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Parcel;
 
 import com.moringaschool.movieworld.R;
+import com.moringaschool.movieworld.adapters.MoviesPagerAdapter;
+import com.moringaschool.movieworld.models.Result;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager) ViewPager mViewPager;
+    private MoviesPagerAdapter adapterViewpager;
+    List<Result> mMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        ButterKnife.bind(this);
+
+        mMovies = Parcels.unwrap(getIntent().getParcelableExtra("movies"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewpager = new MoviesPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mMovies);
+        mViewPager.setAdapter(adapterViewpager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
