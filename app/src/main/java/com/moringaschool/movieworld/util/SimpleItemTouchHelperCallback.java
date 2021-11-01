e511e7a9fc8db7ed582a5aca40f61a30373ecc56
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-11-01 11:52:36 +0300

    Declares and implements the gesture methods that will be used in the app

--- app/src/main/java/com/moringaschool/movieworld/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/moringaschool/movieworld/util/SimpleItemTouchHelperCallback.java
@@ -1,8 +1,15 @@
 package com.moringaschool.movieworld.util;
 
+import android.graphics.Color;
+import android.view.View;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.moringaschool.movieworld.R;
 
 public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
     private final ItemTouchHelperAdapter mAdapter;
@@ -14,6 +21,27 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
     @Override
     public boolean isLongPressDragEnabled(){
         return  true;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled(){
+        return true;
+    }
+
+    @Override
+    public void clearView(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+        viewHolder.itemView.setBackgroundColor(
+                ContextCompat.getColor(viewHolder.itemView.getContext(), R.color.white)
+        );
+    }
+
+    @Override
+    public void onSelectedChanged(@Nullable RecyclerView.ViewHolder viewHolder, int actionState) {
+        super.onSelectedChanged(viewHolder, actionState);
+        if (actionState == ItemTouchHelper.ACTION_STATE_DRAG){
+            viewHolder.itemView.setBackgroundColor(Color.LTGRAY);
+        }
     }
 
     @Override
